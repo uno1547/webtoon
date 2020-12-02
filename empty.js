d17a9f615427396041016462ae842d457c66699b
uno1547 <63580165+uno1547@users.noreply.github.com>
2020-12-02 18:33:23 +0900

    file clean

--- empty.js
+++
@@ -1,9 +0,0 @@
-const color1 = document.querySelector(".color1")
-const color2 = document.querySelector(".color2")
-color1.addEventListener("change", function(){
-    console.log(this.value)
-})
-const number = document.querySelector(".number")
-number.addEventListener("change", function(){
-    console.log(this.value)
-})
